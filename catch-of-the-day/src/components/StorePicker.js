f3c3abb05e232ef2e16e9c769d7f84e9411ec600
Shirasu <akane.shirasu@gmail.com>
2020-05-07 18:09:42 +0900

    add day4

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 class StorePicker extends React.Component {
   render() {
-    return <p>I am the Store Picker!</p>
+    return (
+      <> {/* 複数の兄弟要素を使いたい場合は、空タグを使うか、React.Fragmentを使う */}
+        <form className="store-selector">
+          <h2>Please Enter a Store</h2>
+          <input type="text" required placeholder="Store Name" />
+          <button type="submit">Visit Store →</button>
+        </form>
+      </>
+    )
   }
 }
 
