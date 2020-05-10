c6e841dc657d8cdb970ed67292948402259ea662
Shirasu <akane.shirasu@gmail.com>
2020-05-10 11:49:09 +0900

    add day10

---
+++ catch-of-the-day/src/components/NotFound.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div>
+    <h1>Not Found!!😿</h1>
+  </div>
+)
+export default NotFound;
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
+  goToStore(e) {
+    e.preventDefault();
+    console.log('going');
+  }
   render() {
     return (
       <> {/* 複数の兄弟要素を使いたい場合は、空タグを使うか、React.Fragmentを使う */}
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
           <h2>Please Enter a Store</h2>
-          <input type="text" required placeholder="Store Name" />
+          <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
           <button type="submit">Visit Store →</button>
         </form>
       </>
